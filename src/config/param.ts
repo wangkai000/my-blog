19983f43d2499126c7ca617928d2f6c73a6861d3
wangkai000 <1071231288@qq.com>
2024-12-08 00:21:10 +0800

    feat(v1.0): update+11

--- src/config/param.ts
+++ src/config/param.ts
@@ -13,7 +13,7 @@ export const navFilter = [
 
 export const SITE = {
   title: '天渺工作室的小站',
-  author: '天渺工作室的小站',
+  author: '天渺studio的小站',
   description: '记录 kai 的history',
   profileContent:
         '我是kai，曾创过业。喜欢 Vue、React、Android、Flutter、Nodejs。目前在从事Node全栈，Android开发的工作。',
