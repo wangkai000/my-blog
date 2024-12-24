8d309db025984e2f30f8a15208d06442aeb1090f
wangkai000 <1071231288@qq.com>
2024-12-25 01:30:16 +0800

    feat(v1.0): update+19

---
+++ docs/chunks/Web3-BD9jnpHK.js
@@ -0,0 +1,1 @@
+import{h as o,o as s,c as a}from"../assets/app-CFjPsmNO.js";const e=o({},[["render",function(o,e){return s(),a("div",null,"Web3.0 SmallRecord to do")}]]);export{e as default};
--- src/config/param.ts
+++ src/config/param.ts
@@ -98,9 +98,9 @@ export const giscusSetting = {
   'crossorigin': 'anonymous',
 }
 
-export enum MapboxSetting {
-  // 待替换
-  token = 'pk.eyJ1IjoicGlua3ktcGlnIiwiYSI6ImNsZnJvZ2Q1cDAwZ3ozcG56bXFwbjAzZjAifQ.eEOFvRbKqZHQ3OxeqPBsXw',
-  mapLightStyle = 'mapbox://styles/pinky-pig/clgc32u1e004g01n2bitvj86a',
-  mapDarkStyle = 'mapbox://styles/pinky-pig/clft35kll000o01osk8svnlfg',
-}
+// export enum MapboxSetting {
+//   // 待替换
+//   token = 'pk.eyJ1IjoicGlua3ktcGlnIiwiYSI6ImNsZnJvZ2Q1cDAwZ3ozcG56bXFwbjAzZjAifQ.eEOFvRbKqZHQ3OxeqPBsXw',
+//   mapLightStyle = 'mapbox://styles/pinky-pig/clgc32u1e004g01n2bitvj86a',
+//   mapDarkStyle = 'mapbox://styles/pinky-pig/clft35kll000o01osk8svnlfg',
+// }
